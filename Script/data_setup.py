8f0b0024e30cd1b1316599f13c4d315c591559a9
Gauravsingh564 <emailtogaurav30@gmail.com>
2025-01-16 12:29:10 +0530

    first commit

---
+++ Script/data_setup.py
@@ -0,0 +1,29 @@
+from torchvision import datasets,transforms
+from torch.utils.data import DataLoader
+import os
+NUM_WORKERS=os.cpu_count()
+
+def create_dataloaders(train_dir:str,
+                       test_dir:str,
+                       transform:transforms.Compose,
+                       batch_size:int,
+                       num_workers:int=NUM_WORKERS):
+  train_data=datasets.ImageFolder(root=train_dir,
+                                  transform=transform
+                                  )
+  test_data=datasets.ImageFolder(root=test_dir,
+                                  transform=transform
+                                )
+  class_names=train_data.classes
+  train_dataloader=DataLoader(dataset=train_data,
+                                  batch_size=batch_size,
+                                  num_workers=num_workers,
+                                  shuffle=True,
+                                  pin_memory=True)
+  test_dataloader=DataLoader(dataset=test_data,
+                                  batch_size=batch_size,
+                                  num_workers=num_workers,
+                                  shuffle=False,
+                                  pin_memory=True)
+  return train_dataloader,test_dataloader,class_names
+  
---
+++ Script/prediction.py
@@ -0,0 +1,62 @@
+
+import torch
+import torchvision
+from torchvision import transforms
+import matplotlib.pyplot as plt
+
+from typing import List, Tuple
+
+from PIL import Image
+
+# Set device
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+def pred_and_plot_image(model: torch.nn.Module, 
+                        image_path: str, 
+                        class_names, 
+                        transform,
+                        device: torch.device = device):
+    """Makes a prediction on a target image and plots the image with its prediction."""
+    
+    # 1. Load in image and convert the tensor values to float32
+    target_image = torchvision.io.read_image(str(image_path)).type(torch.float32)
+    
+    # 2. Divide the image pixel values by 255 to get them between [0, 1]
+    target_image = target_image / 255. 
+    
+    # 3. Transform if necessary
+    # Instead of passing the transformed image, pass the transformation pipeline directly
+    if transform is not None and callable(transform):  
+        target_image = transform(target_image) 
+    else:  # Apply a default resize if no transform is provided
+        transform = transforms.Compose([
+            transforms.Resize(size=(64, 64))
+        ])
+        target_image = transform(target_image)
+
+    # 4. Make sure the model is on the target device
+    model.to(device)
+    
+    # 5. Turn on model evaluation mode and inference mode
+    model.eval()
+    with torch.inference_mode():
+        # Add an extra dimension to the image
+        target_image = target_image.unsqueeze(dim=0)
+    
+        # Make a prediction on image with an extra dimension and send it to the target device
+        target_image_pred = model(target_image.to(device))
+        
+    # 6. Convert logits -> prediction probabilities (using torch.softmax() for multi-class classification)
+    target_image_pred_probs = torch.softmax(target_image_pred, dim=1)
+
+    # 7. Convert prediction probabilities -> prediction labels
+    target_image_pred_label = torch.argmax(target_image_pred_probs, dim=1)
+    
+    # 8. Plot the image alongside the prediction and prediction probability
+    plt.imshow(target_image.squeeze().permute(1, 2, 0)) # make sure it's the right size for matplotlib
+    if class_names:
+        title = f"Pred: {class_names[target_image_pred_label.cpu()]} | Prob: {target_image_pred_probs.max().cpu():.3f}"
+    else: 
+        title = f"Pred: {target_image_pred_label} | Prob: {target_image_pred_probs.max().cpu():.3f}"
+    plt.title(title)
+    plt.axis(False);
