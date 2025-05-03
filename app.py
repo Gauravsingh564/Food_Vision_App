2ed130b84b82ce56e1af87d30b94d856722e194b
Gaurav Singh <154744601+Gauravsingh564@users.noreply.github.com>
2025-05-04 01:12:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ IMG_SIZE       = (224, 224)
 NORMALIZE_MEAN = [0.485, 0.456, 0.406]
 NORMALIZE_STD  = [0.229, 0.224, 0.225]
 CLASS_NAMES    = ["pizza", "steak", "sushi"]
-THRESHOLD      = 0.399  # 39.9%
+THRESHOLD      = 0.418  # 39.9%
 
 # ──────────────────────────────────────────────────────────────────────────────
 # 2️⃣ Load model
