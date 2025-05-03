68b8bf1a37f59a9d6b1df1eb974cf0dfa4de1bfc
Gaurav Singh <154744601+Gauravsingh564@users.noreply.github.com>
2025-05-03 11:02:17 +0530

    Update model_builder.py

--- Script/model_builder.py
+++ Script/model_builder.py
@@ -1,55 +1,38 @@
-import torch
-from torch import nn
+import torch.nn as nn
+
 class TinyVGG(nn.Module):
-  def __init__(self, input_shape: int,
-               hidden_units: int,
-               output_shape: int) -> None:
-    super().__init__()
+    def __init__(self,
+                 input_shape: int,
+                 hidden_units: int,
+                 output_shape: int,
+                 pool_size: int = 7):
+        super().__init__()
+        # Feature extractor
+        self.conv_block1 = nn.Sequential(
+            nn.Conv2d(input_shape, hidden_units, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(hidden_units, hidden_units, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+        self.conv_block2 = nn.Sequential(
+            nn.Conv2d(hidden_units, hidden_units * 2, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(hidden_units * 2, hidden_units * 2, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+        # Adaptive pooling to fix spatial dims
+        self.adaptive_pool = nn.AdaptiveAvgPool2d((pool_size, pool_size))
+        # Classifier head
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear((hidden_units * 2) * pool_size * pool_size, output_shape)
+        )
 
-    self.conv_block_1=nn.Sequential(
-        nn.Conv2d(in_channels=input_shape,
-                  out_channels=hidden_units,
-                  kernel_size=3,
-                  stride=1,
-                  padding=0),
-        nn.ReLU(),
-        nn.Conv2d(in_channels=hidden_units,
-                  out_channels=hidden_units,
-                  kernel_size=3,
-                  stride=1,
-                  padding=0),
-        nn.ReLU(),
-        nn.MaxPool2d(kernel_size=2,
-                      stride=2)
-    )
-    self.conv_block_2=nn.Sequential(
-        nn.Conv2d(in_channels=hidden_units,
-                  out_channels=hidden_units,
-                  kernel_size=3,
-                  stride=1,
-                  padding=0),
-        nn.ReLU(),
-        nn.Conv2d(in_channels=hidden_units,
-                  out_channels=hidden_units,
-                  kernel_size=3,
-                  stride=1,
-                  padding=0),
-        nn.ReLU(),
-        nn.MaxPool2d(kernel_size=2,
-                      stride=2)
-    )
-    self.classifier=nn.Sequential(
-        nn.Flatten(),
-        nn.Linear(in_features=hidden_units*13*13,
-                  out_features=output_shape)
-    )
-
-  def forward(self,x):
-    x=self.conv_block_1(x)
-   # print(x.shape)
-    x=self.conv_block_2(x)
-    #print(x.shape)
-    x=self.classifier(x)
-    #print(x.shape)
-    return x
-
+    def forward(self, x):
+        x = self.conv_block1(x)
+        x = self.conv_block2(x)
+        x = self.adaptive_pool(x)
+        x = self.classifier(x)
+        return x
